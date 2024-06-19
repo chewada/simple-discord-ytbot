55ef395d217017293a031b28c13657949a828360
Nour <no4182sa-s@student.lu.se>
2024-06-19 14:44:17 +0200

    Generalized the tests to work on different machines and directories.

--- tests/test_pytube_utils.py
+++ tests/test_pytube_utils.py
@@ -1,9 +1,12 @@
+import os
+import pathlib
+
 import unittest
 from pytube_utils import *
 
 class TestPytubeUtils(unittest.TestCase):
     def test_getAudioFile(self):
-        self.assertEqual(getAudioFile('https://youtube.com/watch?v=2lAe1cqCOXo'), 'C:\\Users\\Nour\\projects\\ytbot\\audio.mp4')
+        self.assertEqual(getAudioFile('https://youtube.com/watch?v=2lAe1cqCOXo'), f'{pathlib.Path(__file__).parent.parent.resolve()}\\audio.mp4')
         self.assertEqual(getAudioFile('This will not work'), None)
     
     def test_isPlaylist(self):
@@ -11,8 +14,9 @@ class TestPytubeUtils(unittest.TestCase):
 
     def test_fastSearch(self):
         self.assertIsNone(fastSearch('https://youtube.com/watch?v=2lAe1cqCOXo'))
-        self.assertEqual(fastSearch('Hello Adele'), 'C:\\Users\\Nour\\projects\\ytbot\\audio.mp4')
+        self.assertEqual(fastSearch('Hello Adele'),  f'{pathlib.Path(__file__).parent.parent.resolve()}\\audio.mp4')
         
 
 if __name__ == '__main__':
     unittest.main()
+    os.remove("audio.mp4")
